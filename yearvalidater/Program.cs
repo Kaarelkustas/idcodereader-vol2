894544d810e620410f589123f495c6cddb048f9a
Opilane TTHK <opilane@tthk.ee>
2020-10-19 14:59:58 +0300

    first commit

---
+++ yearvalidater/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace yearvalidater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter you year of birth");
+            Validate(4);
+            Console.WriteLine("wrong format plz try again");
+        }
+    }
+}
